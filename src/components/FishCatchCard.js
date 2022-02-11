2ea83ad453644e7200cb4e231a8431ca8f1fdf18
Olof Johansson <olaf.sebastian.johansson@gmail.com>
2022-02-12 00:21:50 +0100

    slice lat lon on fishcatchcard

--- src/components/FishCatchCard.js
+++ src/components/FishCatchCard.js
@@ -1,10 +1,14 @@
 const FishCatchCard = ({ fishCatch }) => {
+    const splitPosition = fishCatch.location.split(",");
+    const lat = splitPosition[0].slice(0, 4);
+    const lon = splitPosition[1].slice(0, 4);
+    
     return (
         <section className="fishCatchCard">
                 <h3>{fishCatch.species}</h3>
                 <p>{fishCatch.length} cm</p>
                 <p>{fishCatch.weight} g</p>
-                <p>{fishCatch.location}</p>
+                <p>{lat} {lon}</p>
                 <p className="postDate">{fishCatch.date}</p>
                 <p>Caught by {fishCatch.username}</p>
                 <img src={`http://localhost:8000${fishCatch.imageurl}`} alt={fishCatch.species} />
