13f41982c17850412fffca8bb8cc5f7a6dab3996
Olof Johansson <olaf.sebastian.johansson@gmail.com>
2022-02-18 21:23:11 +0100

    Add search input to list

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import DataContext from "../context/DataContext";
 import Feed from "./Feed";
+import Search from "./Search";
 
 const List = () => {
     const { searchResults, fetchError, isLoading } = useContext(DataContext);
@@ -8,6 +9,7 @@ const List = () => {
     return (
         <article>
             <h1>List</h1>
+            <Search />
             {isLoading && !fetchError && <p className="statusMsg">Loading posts...</p>}
             {fetchError && <p className="statusMsg" style={{color: "red"}}>Could not get data</p>}
             {!isLoading && !fetchError &&
