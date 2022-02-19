61e7686dcbeea24afaa9d73db7735543166db972
Olof Johansson <olaf.sebastian.johansson@gmail.com>
2022-02-19 20:04:11 +0100

    Style toasts

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
+import 'react-toastify/dist/ReactToastify.css';
 import './styles.scss';
 import './dark-leaflet.css';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
-import { toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
   <React.StrictMode>
