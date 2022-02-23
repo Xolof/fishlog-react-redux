95bef7f5dbebe6f0318a53033a2f25d8f6344089
Olof Johansson <olaf.sebastian.johansson@gmail.com>
2022-02-23 21:25:54 +0100

    Move marker components to separate files
    Remove unused variable 'map'

---
+++ src/components/PositionMarker.js
@@ -0,0 +1,28 @@
+import { Marker, useMapEvents } from "react-leaflet";
+
+
+const PositionMarker = ({ location, setLocation, markerLocation, setMarkerLocation }) => {
+  useMapEvents({
+    click(e) {
+      if (setLocation && setMarkerLocation) {
+        setMarkerLocation(e.latlng);
+        setLocation(`${e.latlng.lat},${e.latlng.lng}`);
+      }
+    }
+  });
+
+  if (location && typeof location === "string") {
+    const splitPosition = location.split(",");
+    const lat = splitPosition[0];
+    const lon = splitPosition[1];
+    location = [lat, lon];
+  }
+
+  const position = markerLocation ?? location ?? null;
+
+  return position ? (
+    <Marker position={position}></Marker>
+  ) : null;
+}
+
+export default PositionMarker;
