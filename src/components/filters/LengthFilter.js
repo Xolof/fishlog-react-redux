c61e78abe179b33c1e386e35f323dc2cfa90ab66
Olof Johansson <olaf.sebastian.johansson@gmail.com>
2022-03-08 21:45:48 +0100

    Rename 'gfg' to 'labels' and move to outside of component.

--- src/components/filters/LengthFilter.js
+++ src/components/filters/LengthFilter.js
@@ -1,37 +1,35 @@
 import { Slider } from "@material-ui/core";
 import { useApplicationContext } from "../../context/DataContext";
 
+const labels = [
+  {
+    value: 0,
+    label: "0 cm",
+  },
+  {
+    value: 100,
+    label: "100 cm",
+  },
+  {
+    value: 200,
+    label: "200 cm",
+  },
+  {
+    value: 300,
+    label: "300 cm",
+  },
+  {
+    value: 400,
+    label: "400 cm",
+  },
+  {
+    value: 500,
+    label: "500 cm",
+  },
+];
+
 const LengthFilter = () => {
   const { filterOnLength, setFilterOnLength } = useApplicationContext();
-
-  const gfg = [
-    // ändra till beskrivande namn
-    // Kan läggas utanför komponenten
-    {
-      value: 0,
-      label: "0 cm",
-    },
-    {
-      value: 100,
-      label: "100 cm",
-    },
-    {
-      value: 200,
-      label: "200 cm",
-    },
-    {
-      value: 300,
-      label: "300 cm",
-    },
-    {
-      value: 400,
-      label: "400 cm",
-    },
-    {
-      value: 500,
-      label: "500 cm",
-    },
-  ];
 
   const updateRange = (e, data) => {
     setFilterOnLength(data);
@@ -41,7 +39,7 @@ const LengthFilter = () => {
     <Slider
       value={filterOnLength}
       onChange={updateRange}
-      marks={gfg}
+      marks={labels}
       min={0}
       max={500}
     />
--- src/components/filters/WeightFilter.js
+++ src/components/filters/WeightFilter.js
@@ -1,31 +1,31 @@
 import { Slider } from "@material-ui/core";
 import { useApplicationContext } from "../../context/DataContext";
 
+const labels = [
+  {
+    value: 0,
+    label: "0 g",
+  },
+  {
+    value: 2500,
+    label: "2500 g",
+  },
+  {
+    value: 5000,
+    label: "5000 g",
+  },
+  {
+    value: 7500,
+    label: "7500 g",
+  },
+  {
+    value: 10000,
+    label: "10 000 g",
+  },
+];
+
 const WeightFilter = () => {
   const { filterOnWeight, setFilterOnWeight } = useApplicationContext();
-
-  const gfg = [
-    {
-      value: 0,
-      label: "0 g",
-    },
-    {
-      value: 2500,
-      label: "2500 g",
-    },
-    {
-      value: 5000,
-      label: "5000 g",
-    },
-    {
-      value: 7500,
-      label: "7500 g",
-    },
-    {
-      value: 10000,
-      label: "10 000 g",
-    },
-  ];
 
   const updateRange = (e, data) => {
     setFilterOnWeight(data);
@@ -35,7 +35,7 @@ const WeightFilter = () => {
     <Slider
       value={filterOnWeight}
       onChange={updateRange}
-      marks={gfg}
+      marks={labels}
       min={0}
       max={10000}
     />
