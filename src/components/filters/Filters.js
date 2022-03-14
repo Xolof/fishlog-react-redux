59ad5a179b51c1fe0aab63a348346dc9c0da9a0b
Olof Johansson <olaf.sebastian.johansson@gmail.com>
2022-03-14 20:36:34 +0100

    Make it possible to toggle visibility of filters

--- src/components/filters/Filters.js
+++ src/components/filters/Filters.js
@@ -1,19 +1,41 @@
+import { useState } from "react";
 import SpeciesFilter from "./SpeciesFilter";
 import UserFilter from "./UserFilter";
 import WeightFilter from "./WeightFilter";
 import LengthFilter from "./LengthFilter";
 import DateFilter from "./DateFilter";
+import "../../css/filters.scss";
 
 const Filters = () => {
+  const [showFilters, setShowFilters] = useState(false);
+
   return (
-    <>
-      <h3>Filters</h3>
-      <SpeciesFilter />
-      <UserFilter />
-      <WeightFilter />
-      <LengthFilter />
-      <DateFilter />
-    </>
+    <section className="filters">
+      <div className="filterHeaderWrapper">
+        <h3 className="filterHeader">Filters</h3>
+        <button
+          className={
+            showFilters
+              ? "toggleFiltersButton chevronUp"
+              : "toggleFiltersButton chevronDown"
+          }
+          onClick={() => {
+            setShowFilters(!showFilters);
+          }}
+        ></button>
+      </div>
+      <div
+        className={
+          showFilters ? "filterWrapper fullHeight" : "filterWrapper zeroHeight"
+        }
+      >
+        <SpeciesFilter />
+        <UserFilter />
+        <WeightFilter />
+        <LengthFilter />
+        <DateFilter />
+      </div>
+    </section>
   );
 };
 
