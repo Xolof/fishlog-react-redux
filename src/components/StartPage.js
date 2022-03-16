ecf2f59439a3f9e964b1a93c46afff4e0d184e8e
Olof Johansson <olaf.sebastian.johansson@gmail.com>
2022-03-16 19:35:15 +0100

    Show latest fishCathes on start page

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,7 +1,29 @@
+import { useApplicationContext } from "../context/DataContext";
+import useAxiosFetch from "../hooks/useAxiosFetch";
+import FishCatchCard from "./FishCatchCard";
+
 const StartPage = () => {
+  const API_URL = process.env.REACT_APP_API_URL;
+  useAxiosFetch(`${API_URL}/api/public_fishcatch`);
+
+  const { fetchError, isLoading, fishCatches } = useApplicationContext();
+
   return (
-    <article>
-      <p>Welcome!</p>
+    <article className="startPage">
+      <h2>Latest catches</h2>
+      <div className="fishCatches">
+        {isLoading && !fetchError && <p className="statusMsg">Loading...</p>}
+        {fetchError && <p className="error">Could not get data</p>}
+        {!isLoading &&
+          !fetchError &&
+          fishCatches
+            .slice(0, 4)
+            .map((fishCatch) => (
+              <FishCatchCard fishCatch={fishCatch} key={fishCatch.id} />
+            ))}
+      </div>
+      <h2>Your position</h2>
+      <h2>Current weather</h2>
     </article>
   );
 };
