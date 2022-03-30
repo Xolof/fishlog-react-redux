4a963754032b9ce1c9a74040587747eb8b5d0bcd
Olof Johansson <olaf.sebastian.johansson@gmail.com>
2022-03-31 00:04:34 +0200

    Register dataSlice in the store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../slices/userSlice";
 import themeReducer from "../slices/themeSlice";
+import dataReducer from "../slices/dataSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     theme: themeReducer,
+    data: dataReducer,
   },
 });
