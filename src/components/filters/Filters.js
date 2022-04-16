2a1fe829c69413dfc4c362e00e79925a60f8bade
Olof Johansson <olaf.sebastian.johansson@gmail.com>
2022-04-16 19:14:57 +0200

    Adjust style of sortinputs

--- src/components/filters/Filters.js
+++ src/components/filters/Filters.js
@@ -6,7 +6,6 @@ import LengthFilter from "./LengthFilter";
 import DateFilter from "./DateFilter";
 import Sort from "./Sort";
 import { useLocation } from "react-router-dom";
-import "../../css/filters.scss";
 
 const Filters = () => {
   const [showFilters, setShowFilters] = useState(false);
