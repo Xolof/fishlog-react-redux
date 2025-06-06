9a73755e8aa4abdee6c6b243146dbb9cfa2b2de3
Olof Johansson <olof@liberdev.se>
2025-06-06 09:05:08 +0200

    Fix HTML on About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,31 +1,32 @@
 const About = () => {
   return (
     <article>
+      <h1>About Fishlog</h1>
       <p>
         This is an app for logging fish you caught. The the frontend is made
         with React and Redux and the backend is made with Laravel.
-        <h2>GitHub Repositories</h2>
-        <ul>
-          <li>
-            <a
-              href="https://github.com/Xolof/fishlog-react-redux"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Frontend
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/Xolof/fishlog-backend"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Backend
-            </a>
-          </li>
-        </ul>
       </p>
+      <h2>GitHub Repositories</h2>
+      <ul>
+        <li>
+          <a
+            href="https://github.com/Xolof/fishlog-react-redux"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Xolof/fishlog-backend"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Backend
+          </a>
+        </li>
+      </ul>
     </article>
   );
 };
