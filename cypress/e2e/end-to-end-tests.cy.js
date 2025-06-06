61be23d34a4737c6be95b9fc01571c1f8a215c2c
Olof Johansson <olof@liberdev.se>
2025-06-06 19:39:57 +0200

    restructure Cypress end to end tests

--- cypress/e2e/end-to-end-tests.cy.js
+++ cypress/e2e/end-to-end-tests.cy.js
@@ -1,69 +1,64 @@
 import "cypress-file-upload";
 
-const URL = `http://localhost:3000/map/all`;
+const URL = `http://localhost:3000`;
+const email = "kjell@ullared.se";
+const password = "pass123";
+const species = "mackerel"
+const imageFile = "mackerel.jpg";
+const weight = 500;
+const length = 42;
+const date = getFormattedDate();
 
-// const species = "torsk";
+function logIn() {
+  cy.get("a:contains(Login)").click();
+  cy.get("#email").type(email);
+  cy.get("#passWord").type(password);
+  cy.get("button:contains(Log in)").click();
+}
 
-// describe("Test filtering for a species", () => {
-//   it(`Visits ${URL}`, () => {
-//     cy.visit(URL);
-//     cy.get("#species").type(species);
-//     cy.get(".leaflet-marker-icon").first().click();
-//     cy.get(".leaflet-popup-content-wrapper").contains(species);
-//   });
-// });
+function logOut() {
+  cy.get("button:contains(Logout)").click();
+  cy.get(".Toastify__toast").contains("You have been logged out");
+}
 
-// describe("Test going to Add Page without logging in", () => {
-//   it(`Visits ${URL}`, () => {
-//     cy.visit(URL);
-//     cy.get("a:contains(Add)").click();
-//     cy.contains("Login to be able to add a catch");
-//   });
-// });
+function getFormattedDate() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const formattedDate = `${year}-${month}-${day}`;
+  return formattedDate;
+}
 
-// describe("Test logging in, then logging out", () => {
-//   it(`Visits ${URL}`, () => {
-//     cy.visit(URL);
-//     cy.get("a:contains(Login)").click();
-//     cy.get("#email").type("kjell@ullared.se");
-//     cy.get("#passWord").type("pass123");
-//     cy.get("button:contains(Log in)").click();
-//     cy.get(".Toastify__toast-body").contains("You logged in");
-//     cy.get("button:contains(Logout)").click();
-//     cy.get(".Toastify__toast-body").contains("You have been logged out");
-//   });
-// });
-
-describe("Test logging in, adding a fishcatch, deleting it and logging out.", () => {
-  it(`Visits ${URL}`, () => {
+describe("User workflow.", () => {
+  it("Login, add fishCatch, delete fishCatch, logout.", () => {
     cy.viewport(1024, 768);
     cy.visit(URL);
-    cy.get("a:contains(Login)").click();
-    cy.get("#email").type("kjell@ullared.se");
-    cy.get("#passWord").type("pass123");
-    cy.get("button:contains(Log in)").click();
+    logIn();
     cy.get(".Toastify__toast").contains("You logged in");
+    cy.get(".Toastify__close-button").click();
     cy.get("a:contains(Add)").click();
-    cy.get("#species").type("kjellsTest");
-    cy.get("#weight").type("500");
-    cy.get("#length").type("45");
-    cy.get("#date").type("2022-02-05");
     cy.get(".leaflet-container").click(400, 250);
-    const imageFile = "mackerel.jpg";
+    cy.get("form.addForm").find('#species', { timeout: 1000 }).should('be.visible');
+    cy.get("form.addForm").find("#species").type(species);
+    cy.get("#weight").type(weight);
+    cy.get("#length").type(length);
+    cy.get("#date").type(date);
     cy.get("#uploadImage").attachFile(imageFile);
     cy.get("button:contains(Save)").click();
-    cy.visit("http://localhost:3000/list");
+    cy.get("a:contains(List)").click();
     cy.get(".toggleFiltersButton").click();
-    cy.get("#species").type("kjellsTest");
-    cy.contains("kjellsTest");
-    cy.contains("500 g");
-    cy.contains("45 cm");
-    cy.contains("2022-02-05");
-    cy.get(".deleteButton").click();
+    cy.get("#species").type(species);
+    cy.contains(species);
+    cy.contains(weight + " g");
+    cy.contains(length + " cm");
+    cy.contains(date);
+    cy.get(".deleteButton").first().click();
     cy.wait(500);
     cy.get(".Toastify__toast").contains("Catch deleted");
-    cy.wait(1000);
-    cy.get("button:contains(Logout)").click();
-    cy.get(".Toastify__toast").contains("You have been logged out");
+    cy.get(".Toastify__close-button").click({ multiple: true });
+    logOut();
   });
 });
+
+
