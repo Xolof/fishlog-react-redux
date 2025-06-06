81a0a7da7ddef9cb3a632004817dcd3f6430bd03
Olof Johansson <olof@liberdev.se>
2025-06-06 16:21:12 +0200

    Replace switch with if statements because eslint does not seem to work well with the switch

--- src/slices/dataSlice.js
+++ src/slices/dataSlice.js
@@ -132,61 +132,61 @@ export const selectSearchResultsSelector = createSelector(
       );
     });
 
-    switch (sortBy) {
-      case "species":
-      case "username":
-        filteredResults.sort((a, b) => {
-          if (sortOrder === "DESC") {
-            const stringA = a[sortBy].toUpperCase();
-            const stringB = b[sortBy].toUpperCase();
-            if (stringA < stringB) {
-              return -1;
-            }
-            if (stringA > stringB) {
-              return 1;
-            }
-            return 0;
+    if (sortBy === "username" || sortBy === "species") {
+      filteredResults.sort((a, b) => {
+        if (sortOrder === "DESC") {
+          const stringA = a[sortBy].toUpperCase();
+          const stringB = b[sortBy].toUpperCase();
+          if (stringA < stringB) {
+            return -1;
           }
-          if (sortOrder === "ASC") {
-            const stringA = a[sortBy].toUpperCase();
-            const stringB = b[sortBy].toUpperCase();
-            if (stringA > stringB) {
-              return -1;
-            }
-            if (stringA < stringB) {
-              return 1;
-            }
-            return 0;
+          if (stringA > stringB) {
+            return 1;
           }
-        });
-        break;
-      case "weight":
-      case "length":
-        filteredResults.sort((a, b) => {
-          if (sortOrder === "DESC") {
-            return (
-              parseInt(b[sortBy]) - parseInt(a[sortBy])
-            );
+          return 0;
+        }
+        if (sortOrder === "ASC") {
+          const stringA = a[sortBy].toUpperCase();
+          const stringB = b[sortBy].toUpperCase();
+          if (stringA > stringB) {
+            return -1;
           }
-          if (sortOrder === "ASC") {
-            return (
-              parseInt(a[sortBy]) - parseInt(b[sortBy])
-            );
+          if (stringA < stringB) {
+            return 1;
           }
-        });
-        break;
-      case "date":
-        filteredResults.sort((a, b) => {
-          const aTime = new Date(a[sortBy]).getTime();
-          const bTime = new Date(b[sortBy]).getTime();
-          if (sortOrder === "DESC") {
-            return parseInt(bTime) - parseInt(aTime);
-          }
-          if (sortOrder === "ASC") {
-            return parseInt(aTime) - parseInt(bTime);
-          }
-        });
-        break;
+          return 0;
+        }
+      });
+    }
+
+    if (sortBy === "length" || sortBy === "weight") {
+      filteredResults.sort((a, b) => {
+        if (sortOrder === "DESC") {
+          return (
+            parseInt(b[sortBy]) - parseInt(a[sortBy])
+          );
+        }
+        if (sortOrder === "ASC") {
+          return (
+            parseInt(a[sortBy]) - parseInt(b[sortBy])
+          );
+        }
+      });
+
+    }
+
+    if (sortBy === "date") {
+      filteredResults.sort((a, b) => {
+        const aTime = new Date(a[sortBy]).getTime();
+        const bTime = new Date(b[sortBy]).getTime();
+        if (sortOrder === "DESC") {
+          return parseInt(bTime) - parseInt(aTime);
+        }
+        if (sortOrder === "ASC") {
+          return parseInt(aTime) - parseInt(bTime);
+        }
+      });
+
     }
 
     if (filterOnDateFrom !== "") {
