1c7a0d398ae24a40420bcbf6a75578a0f4d5a2ba
Xolof <olof@liberdev.se>
2025-06-21 11:43:28 +0200

    center map on the FishCatch when editing

--- src/components/addedit/AddEditMap.tsx
+++ src/components/addedit/AddEditMap.tsx
@@ -4,6 +4,9 @@ import PositionMarker from "../markers/PositionMarker";
 import { useSelector } from "react-redux";
 import { selectUserLat, selectUserLng } from "../../slices/userSlice";
 import { selectTileUrl } from "../../slices/themeSlice";
+import { selectFishCatches } from "../../slices/dataSlice";
+import { useParams } from "react-router-dom";
+import { FishCatch } from "../../types/FishCatch";
 
 type AddEditMapProps = {
   center: number[] | null;
@@ -13,13 +16,32 @@ const AddEditMap: React.FC<AddEditMapProps> = () => {
   const userLat = useSelector(selectUserLat) as number | null;
   const userLng = useSelector(selectUserLng) as number | null;
   const tileUrl = useSelector(selectTileUrl) as string;
+  const fishCatches = useSelector(selectFishCatches);
 
   let userCoordinates: [number, number] | null = null;
   if (typeof userLat === "number" && typeof userLng === "number") {
     userCoordinates = [userLat, userLng];
   }
 
-  const mapCenter: [number, number] = userCoordinates ?? [56, 12.6];
+  let mapCenter: [number, number] = userCoordinates ?? [56, 12.6];
+
+  const { id } = useParams<{ id?: string }>();
+
+  if (id) {
+    const numericId = Number(id);
+
+    const currentFishCatch = fishCatches.filter(
+      (item: FishCatch) => item.id == numericId
+    )[0];
+
+    if (currentFishCatch) {
+      const splitLocation = currentFishCatch.location.split(",");
+      const lat = parseFloat(splitLocation[0]);
+      const lon = parseFloat(splitLocation[1]);
+      console.log(lat, lon);
+      mapCenter = [lat, lon];
+    }
+  }
 
   return (
     <>
